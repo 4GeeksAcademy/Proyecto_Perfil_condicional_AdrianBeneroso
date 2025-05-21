8fa9c8cb757cf5812ca3c57b7ab629923f800521
AdrianBene <adrianbeneroso@hotmail.com>
2025-05-21 09:48:15 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,43 @@
 import "../style/index.css";
-
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let newName = !variables.name ? "Name" : variables.name;
+  let newLastName = !variables.lastName ? "Last name" : variables.lastName; // reset the website body with the new html output
+  let newRole = !variables.role ? "role" : variables.role;
+  let newCity = !variables.city ? "city" : variables.city;
+  let newCountry = !variables.country ? "Country" : variables.country;
+  let newTwitter = !variables.twitter
+    ? "https://x.com/?lang=es"
+    : variables.twitter;
+  let newGitHub = !variables.github ? "https://github.com/" : variables.github;
+  let newLinkedin = !variables.linkedin
+    ? "https://linkedin.com/school/4geeksacademy"
+    : variables.linkedin;
+  let newInstagram = !variables.instagram
+    ? "https://www.instagram.com/"
+    : variables.instagram;
 
-  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${newName} ${newLastName}</h1>
+          <h2>${newRole}</h2>
+          <h3>${newCity}, ${newCountry}</h3>
+          <ul class="${
+            variables.socialMediaPosition === "position-right"
+              ? "position-right"
+              : "position-left"
+          }">
+            <li><a href="${newTwitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${newGitHub}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${newLinkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${newInstagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -71,7 +63,6 @@ window.onload = function() {
     city: null
   };
   render(window.variables); // render the card for the first time
-
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
       // <- add a listener to every input
